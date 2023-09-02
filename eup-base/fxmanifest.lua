264b0fea478c4691046d5c6de13c46952a7affd4
Vexxys Development <122513007+VexxyDevs@users.noreply.github.com>
2023-09-02 22:07:39 +0100

    Add files via upload

---
+++ eup-base/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'vexxy'
+description 'eup-base'
+version 'v1'
+
+server_scripts {
+    's/server.lua',
+    
+    
+}
+
+
+
+client_scripts {
+    's/client.lua'
+}
